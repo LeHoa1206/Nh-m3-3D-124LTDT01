06bd5c19e6cba0f4d833e96acce2f0b1b2e10fd2
TranXuan2212 <tranduonghuuxuan22@gmail.com>
2024-12-21 08:53:15 +0700

    update connect DB

---
+++ connectDB/db_connection.php
@@ -0,0 +1,15 @@
+<?php
+// Kết nối cơ sở dữ liệu
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "baitapnhom";
+
+// Tạo kết nối
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Kết nối thất bại: " . $conn->connect_error);
+}
+?>
